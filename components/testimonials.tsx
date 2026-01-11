bcb0fb18cad9777f31d60ebb0e75b10a643bdd9f
eszter-with-a-z <eszter.with.a.z@gmail.com>
2026-01-11 23:09:05 +0100

    Redesign testimonials 2

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -7,9 +7,9 @@ export default function Testimonials() {
     <section id="testimonials" className="py-24 bg-accent">
       <div className="container mx-auto px-6">
         <h2 className="sticky top-24  text-4xl font-bold mb-16 text-center text-white">Testimonials</h2>
-        <div className="relative flex flex-col gap-24 gap-8">
+        <div className="relative flex flex-col items-center gap-24 gap-8">
           {testimonials.map((testimonial, index) => (
-            <Card key={index} className="sticky top-24 bg-background transition-shadow">
+            <Card key={index} className="sticky top-24 max-w-[400px] bg-background transition-shadow">
               <CardContent className="p-6 pt-0">
                 <p className="mb-6 font-semibold text-foreground/50">{testimonial.id}/{testimonials.length}</p>
                 <p className="text-muted-foreground mb-6 leading-relaxed italic">{renderWithTextStyling(testimonial.text, testimonial.bold)}</p>
