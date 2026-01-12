02d8ff1c2572e4db522c319f9f5a8abc7c335dfd
eszter-with-a-z <eszter.with.a.z@gmail.com>
2026-01-12 11:07:35 +0100

    Sorry, fixing it here

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -9,7 +9,7 @@ export default function Testimonials() {
         <h2 className="sticky top-24  text-4xl font-bold mb-16 text-center text-white">Testimonials</h2>
         <div className="relative flex flex-col items-center gap-24 gap-8">
           {testimonials.map((testimonial, index) => (
-            <Card key={index} className="sticky top-24 max-w-[400px] bg-background transition-shadow">
+            <Card key={index} className="sticky top-36 max-w-[400px] bg-background transition-shadow">
               <CardContent className="p-6 pt-0">
                 <p className="mb-6 font-semibold text-foreground/50">{testimonial.id}/{testimonials.length}</p>
                 <p className="text-muted-foreground mb-6 leading-relaxed italic">{renderWithTextStyling(testimonial.text, testimonial.bold)}</p>
