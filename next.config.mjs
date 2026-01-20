24b7a208abf18bd1551cd70f5c96dfbb0854658f
eszter-with-a-z <eszter.with.a.z@gmail.com>
2026-01-20 11:00:08 +0100

    Remove basepath in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,8 @@ const nextConfig = {
   
   // If deploying to username.github.io (root domain), remove these two lines
   // If deploying to username.github.io/repo-name, uncomment and set to '/repo-name'
-  basePath: '/eszter-with-sz-portfolio',
-  assetPrefix: '/eszter-with-sz-portfolio',
+  //basePath: '/eszter-with-sz-portfolio',
+  //assetPrefix: '/eszter-with-sz-portfolio',
 }
 
 export default nextConfig
